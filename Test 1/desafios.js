e4fc5507ab62c0292d11c2ef87735b6a1cdfedc9
John Bulla <156612466+Nethfly@users.noreply.github.com>
2024-03-08 18:37:51 -0500

    1

---
+++ Test 1/desafios.js
@@ -0,0 +1,18 @@
+"use strict"
+//Hacer un programa que salude
+/* console.log("Hola")
+document.write("Hola") */
+
+// Hacer programa que salude por el nombre, si no recibe nobre solo saludar
+
+const entrada = prompt ("Enter your name")
+console.log (entrada);
+
+switch(entrada){
+    case "":
+        document.write("Hello There!");
+        break;
+    default:
+        document.write("Hello There "+ entrada + " Welcome");
+        break;            
+}

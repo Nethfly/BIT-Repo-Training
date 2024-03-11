88648f0ec790cf1a26992e0b3b8ff9db883ad9d6
John Bulla <156612466+Nethfly@users.noreply.github.com>
2024-03-10 20:40:13 -0500

    other

--- Test 1/desafios1.js
+++ Test 1/desafios1.js
@@ -16,15 +16,3 @@ switch(entrada){
         document.write("Hello There! "+ entrada + ", Welcome to the Jedi Council");
         break;          
 }*/
-
-const userName = prompt("Enter your name")
-
-// Check if the user provided a name
-if(userName === ""){
-    //If not, greet them as "Youngling"
-        document.write("Hello There! Youngling");
-    } else {
-        // If they did provide a name, greet them with a welcome message
-          document.write('Hello There! ${userName}, Welcome to the Jedi Council');
-   
-}
